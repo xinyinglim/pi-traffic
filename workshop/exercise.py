bab44a6bc9dbf1629f298364e40210915ff18efd
Xin Ying Lim <springflight@gmail.com>
2019-10-15 22:55:08 +0800

    Add exercise sheet

---
+++ workshop/exercise.py
@@ -0,0 +1,19 @@
+# 1. import gpiozero's LED and Button and sleep
+
+# 2. declare lights : red, yellow, green, blue
+# eg: red light at gpio 4:
+# red = LED(4)
+
+
+# 3. switch on and off red light
+# switch on yellow light, then green light
+# do a full loop of a traffic light
+
+
+# 4. wait for button press, then update variable "pedestrian_pressed"
+
+# 5. In a while loop, run traffic light
+# if pedestrian_pressed is true, stop and run the blue pedestrian light
+# then continue
+
+
